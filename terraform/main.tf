4aef603b6d2fa6be92345d8ed44da5be3208bbd3
KamalPurohit <kamal@example.com>
2026-05-25 06:25:31 +0530

    removed S3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,29 +1,23 @@
 
-# S3 bucket module
-module "s3" {
-  source      = "./modules/s3"
-  bucket_name = "kamal-demo-bucket"
-}
-
 # VPC module (fetches default VPC + subnets)
-module "vpc" {
-  source = "./modules/vpc"
+module "VPC" {
+  source = "./VPC"
 }
 
 # Security group module
-module "sg" {
-  source = "./modules/security_group"
+module "security_group" {
+  source = "./security_group"
   vpc_id = module.vpc.vpc_id
 }
 
 # IAM role module
-module "iam" {
-  source = "./modules/iam"
+module "IAM_Role" {
+  source = "./IAM_Role"
 }
 
 # EC2 module (creates 2 Ubuntu instances)
-module "ec2" {
-  source           = "./modules/ec2"
+module "EC2" {
+  source           = "./EC2"
   subnet_ids       = module.vpc.subnet_ids
   sg_id            = module.sg.sg_id
   instance_profile = module.iam.instance_profile
