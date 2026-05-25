4cb2bad95f1604a1d10199f303ff6a863bd6ba51
KamalPurohit <kamal@example.com>
2026-05-25 06:40:41 +0530

    new chnages

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ module "IAM_Role" {
 # EC2 module (creates 2 Ubuntu instances)
 module "EC2" {
   source           = "./EC2"
-  subnet_ids       = module.VPC.subnet_ids
+  subnet_ids       = module.VPC.subnet_id
   sg_id            = module.security_group.sg_id
   instance_profile = module.IAM_Role.instance_profile
 }
