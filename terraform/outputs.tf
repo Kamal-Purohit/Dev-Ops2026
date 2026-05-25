450e4f1ad6b173506952bda62fdbd975b9103d8d
KamalPurohit <kamal@example.com>
2026-05-25 06:35:16 +0530

    changes completed

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
 output "ec2_instance_ids" {
-  value = module.ec2.instance_ids
+  value = module.EC2.instance_ids
 }
 
 output "ec2_public_ips" {
-  value = module.ec2.public_ips
+  value = module.EC2.public_ips
 }
